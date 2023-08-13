a8e6ef7dec21abb8478cac26257958a99794a15c
sudipto976 <sudiptoghosh9767@gmail.com>
2023-08-13 11:21:08 +0530

    few test added1

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,46 +8,46 @@ import prediction_service
 
 input_data = {
     "incorrect_range":
-        {"fixed_acidity": 7897897,
-         "volatile_acidity": 555,
-         "citric_acid": 99,
-         "residual_sugar": 99,
-         "chlorides": 12,
-         "free_sulfur_dioxide": 789,
-         "total_sulfur_dioxide": 75,
-         "density": 2,
-         "pH": 33,
-         "sulphates": 9,
-         "alcohol": 9
-         },
+    {"fixed_acidity": 7897897,
+     "volatile_acidity": 555,
+     "citric_acid": 99,
+     "residual_sugar": 99,
+     "chlorides": 12,
+     "free_sulfur_dioxide": 789,
+     "total_sulfur_dioxide": 75,
+     "density": 2,
+     "pH": 33,
+     "sulphates": 9,
+     "alcohol": 9
+     },
 
-        "correct_range":
-        {"fixed_acidity": 5,
-         "volatile_acidity": 1,
-         "citric_acid": 0.5,
-         "residual_sugar": 10,
-         "chlorides": 0.5,
-         "free_sulfur_dioxide": 3,
-         "total_sulfur_dioxide": 75,
-         "density": 1,
-         "pH": 3,
-         "sulphates": 1,
-         "alcohol": 9
-         },
+    "correct_range":
+    {"fixed_acidity": 5,
+     "volatile_acidity": 1,
+     "citric_acid": 0.5,
+     "residual_sugar": 10,
+     "chlorides": 0.5,
+     "free_sulfur_dioxide": 3,
+     "total_sulfur_dioxide": 75,
+     "density": 1,
+     "pH": 3,
+     "sulphates": 1,
+     "alcohol": 9
+     },
 
-        "incorrect_col":
-        {"fixed acidity": 5,
-         "volatile acidity": 1,
-         "citric acid": 0.5,
-         "residual sugar": 10,
-         "chlorides": 0.5,
-         "free sulfur dioxide": 3,
-         "total sulfur dioxide": 75,
-         "density": 1,
-         "pH": 3,
-         "sulphates": 1,
-         "alcohol": 9
-         },
+    "incorrect_col":
+    {"fixed acidity": 5,
+     "volatile acidity": 1,
+     "citric acid": 0.5,
+     "residual sugar": 10,
+     "chlorides": 0.5,
+     "free sulfur dioxide": 3,
+     "total_sulfur dioxide": 75,
+     "density": 1,
+     "pH": 3,
+     "sulphates": 1,
+     "alcohol": 9
+     }
 }
 
 TARGET_range = {
