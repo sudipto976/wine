fe86e5f4f3fdea2471440896bc250408fbda7b82
sudipto976 <sudiptoghosh9767@gmail.com>
2023-08-13 09:05:00 +0530

    tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,10 @@
+import json
+import logging
+import os
+import joblib
 import pytest
+from prediction_service.prediction import form_response, api_response
+import prediction_service
 
 
 class NotInRange(Exception):
